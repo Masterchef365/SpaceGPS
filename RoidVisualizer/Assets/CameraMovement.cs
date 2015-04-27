9fe8b0d7338f2a1330d67469080abe1d2c826d93
Duncan Freeman <duncan.freeman1@gmail.com>
2015-04-26 22:26:27 -0700

    Imported To Github
    ALL THE THINGS

---
+++ RoidVisualizer/Assets/CameraMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			Vector3 newRot = new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) + transform.rotation.eulerAngles;
+			transform.rotation = Quaternion.Euler (newRot);
+		}
+		float speed;
+		if (Input.GetKey("left shift")){
+			speed = 2f;
+		} else {
+			speed = 1f;
+		}
+		
+		if (Input.GetKey ("w")) {
+			transform.Translate(Vector3.forward*speed, Space.Self);
+		}
+		else
+		{
+			if (Input.GetKey ("s")) {
+				transform.Translate(Vector3.back*speed, Space.Self);
+			}
+		}
+		
+		if (Input.GetKey ("a")) {
+			transform.Translate(Vector3.left*speed,Space.Self);
+		}
+		else
+		{
+			if (Input.GetKey ("d")) {
+				transform.Translate(Vector3.right*speed,Space.Self);
+			}
+		}
+		
+		if (Input.GetKey ("space")) {
+			transform.Translate(Vector3.up*speed,Space.Self);
+		}
+		else
+		{
+			if (Input.GetKey ("left ctrl")) {
+				transform.Translate(Vector3.down*speed,Space.Self);
+			}
+		}
+	}
+}
---
+++ RoidVisualizer/Assets/ImportRoids.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+
+public class ImportRoids : MonoBehaviour {
+	public GameObject prefab;
+	public List<string> textElements = new List<string>();
+	public List<Vector3> posElements = new List<Vector3>();
+
+	void Start () {
+		ArrayList coordList = new ArrayList ();
+		FileStream f = new FileStream("gps.txt", FileMode.Open);
+		StreamReader reader = new StreamReader(f);
+		while (true)
+		{
+			string line = reader.ReadLine();
+			if (line == "")
+			{
+				break;
+			}
+			else
+			{
+				coordList.Add(line);
+			}
+		}
+		for (int i = 0; i < coordList.Count; i++)
+		{
+			string[] coord = coordList[i].ToString().Split(':');
+			float x,y,z;
+			string name = coord[1];
+			float.TryParse(coord[2], out x);
+			float.TryParse(coord[3], out y);
+			float.TryParse(coord[4], out z);
+			Vector3 pos = new Vector3(x/10000f,y/10000f,z/10000f);
+			GameObject roid = (GameObject)GameObject.Instantiate(prefab.gameObject, pos, Quaternion.identity);
+			roid.name = name;
+			textElements.Add(name);
+			posElements.Add(pos);
+		}
+	}
+
+	void OnGUI () {
+		for (int i = 0; i < textElements.Count; i ++) {
+			Vector3 actualPos = posElements[i];
+			Vector3 marker = gameObject.GetComponent<Camera>().WorldToScreenPoint(actualPos);
+			if () {
+				GUI.Label(new Rect(marker.x, Screen.height - marker.y, 100,100), textElements[i]);
+			}
+		}
+	}
+
+}
